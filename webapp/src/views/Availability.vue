d6df9ded13bdb48701c2f7ba73bbdf5237b3bbf3
alex855k <alexander2341@gmail.com>
2019-04-24 21:13:25 -0700

    (WebApp-US01-InterviewerUpdateAvaliability) Add calendar and setup base vue markup for avalibility page.

--- webapp/src/views/Availability.vue
+++ webapp/src/views/Availability.vue
@@ -1,13 +1,19 @@
 <template>
-  <v-container
-    fill-height
-    fluid
-    grid-list-xl
-  >
-    <v-layout wrap>
-   
-    </v-layout>
-  </v-container>
+ <v-container fluid fill-height grid-list-md text-xs-center>
+  <v-layout column wrap>
+    <v-calendar />
+  </v-layout>
+  <v-layout columnn wrap>
+
+     <v-container grid-list-md text-xs-center>
+        <v-layout row wrap>
+          <v-flex v-for="i in " :key="`6${i}`" xs6>
+            <v-btn color="success">10</v-btn>
+          </v-flex>
+        </v-layout>
+      </v-container>
+  </v-layout>
+</v-container>
 </template>
 
 <script>
